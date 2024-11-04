1d404fca03403b540066a5af44aee8de4d06e668
milindkusahu <milindsahu2000@gmail.com>
2024-11-05 04:31:01 +0530

    Fix: runtime error by using dynamic imports

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,23 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
+// import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
+// import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+import dynamic from "next/dynamic";
+
+const RecentProjects = dynamic(() => import("../components/RecentProjects"), {
+  ssr: false,
+});
+
+const Grid = dynamic(() => import("../components/Grid"), {
+  ssr: false,
+});
 
 const Home = () => {
   return (
@@ -17,7 +27,7 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        {/* <Grid /> */}
+        <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
